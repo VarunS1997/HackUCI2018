1c37d2cc6272f378fc5dd8bbc38741bd7da8b67d
Varun Singh <varun.1997.singh@gmail.com>
2018-02-03 01:56:21 -0800

    Fixed links TEMPORARILY

---
+++ about.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>About | Sanatree</title>
+
+    <?php require "PHP/stdConfig.php"; ?>
+
+    <style type="text/css">
+
+    </style>
+</head>
+<body onload="initResponsive()" onresize="initResponsive()">
+    <?php require_once "HTML/navbar.html"; ?>
+    <div class="wrapper" id="body-wrapper">
+        <div class="subsection-wrapper wrapper">
+            <div class="subsection">
+                <div class="title">Fuck you</div>
+                <p>
+                    And your sister(s).
+                </p>
+            </div>
+        </div>
+    </div>
+    <?php require_once "PHP/footer.php"; ?>
+</body>
+</html>
