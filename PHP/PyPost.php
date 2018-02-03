0c2df8ac446fb9ceec6415ff59b90c5dabe23b83
Varun Singh <varun.1997.singh@gmail.com>
2018-02-03 08:22:36 -0800

    Added a py script and private policy

--- PHP/PyPost.php
+++ PHP/PyPost.php
@@ -1,3 +1,12 @@
 <?php
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $jsonData = file_get_contents('php://input');
 
+    $tempAdd = "TEMP/IMPORT_DATA.json";
+    $tempFile = fopen($tempAdd, "w+");
+    fwrite($tempFile, $jsonData);
+    fclose($tempFile);
+
+    $pyOut = passthru("python pyScripts/Json_to_Sql.py " . $start_word . " " . $end_word);
+}
 ?>
